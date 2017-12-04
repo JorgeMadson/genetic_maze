6a11d22a54d7285cffa93f54c2521108d4f0ae12
Jorge Madson <filholouco@gmail.com>
2017-12-04 17:30:40 -0200

    Tudo num arquivo só

--- genetic_maze.c
+++ genetic_maze.c
@@ -1,25 +1,67 @@
-/*
-	TODO
-	- use qsort instead of bubble sort
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <math.h>
 
-#include "genetic_maze.h"
+#define LIMITE_MOVIMENTACAO 25
+#define MAZE_X 10
+#define MAZE_Y 10
+
+/*
+Definição das funções para funcionamento do software
+*/
 
 
-int array[POPULATION_SIZE][MOVE_LIMIT];
-int best_spec[BEST_CNT][MOVE_LIMIT];
+/* Parametros sobre a população */
+#define TAM_POPULACAO 500
+#define BEST_CNT 7
+#define REPRODUCTIVE 150
+#define PENALTY_VAL 2
+#define MUTATION_CHANCE 0.5f
+#define FINAL_SCORE 3
 
-point_t finish_pos;
-point_t player;
+
+typedef struct{
+	int x;
+	int y;
+}ponto_t;
+
+typedef struct{
+	int score;
+	int index;
+}pontuacao_t;
+
+
+
+void print_maze();
+void print_maze_path();
+
+void player_reset();
+void player_stay();
+void player_move_up();
+void player_move_down();
+void player_move_left();
+void player_move_right();
+void fill();
+int* sort(int* array, int size);
+void sort_pontuacao_t();
+void cross();
+int fitness(int chromo);
+int score();
+void move_player();
+void mutate();
+
+
+
+int array[TAM_POPULACAO][LIMITE_MOVIMENTACAO];
+int best_spec[BEST_CNT][LIMITE_MOVIMENTACAO];
+
+ponto_t finish_pos;
+ponto_t player;
 int penalty;
 
-scores_t scores[POPULATION_SIZE];
+pontuacao_t scores[TAM_POPULACAO];
 
 void (*func_ptr[5])() = {player_stay, player_move_up, player_move_down, player_move_left, player_move_right};
 
@@ -41,7 +83,7 @@ void print_maze_path(){
 	char temp[MAZE_X][MAZE_Y] = {};
 	memcpy(temp, maze, MAZE_X*MAZE_Y);
 	int i;
-	for(i = 0; i < MOVE_LIMIT; i++){
+	for(i = 0; i < LIMITE_MOVIMENTACAO; i++){
 		if(array[scores[0].index][i]){
 			func_ptr[array[scores[0].index][i]](); // best score path
 			maze[player.x][player.y] = '*';//(char)(i+97);
@@ -131,8 +173,8 @@ void player_move_right(){
 
 void fill(){
     int x,y;
-	for(x = 0; x < POPULATION_SIZE; x++){
-		for(y = 0; y < MOVE_LIMIT; y++){
+	for(x = 0; x < TAM_POPULACAO; x++){
+		for(y = 0; y < LIMITE_MOVIMENTACAO; y++){
 			array[x][y] = rand() % 5;
 		}
 	}
@@ -153,12 +195,12 @@ int* sort(int* array, int size){
 	return array;
 }
 
-void sort_scores_t(){
+void sort_pontuacao_t(){
     int x,i;
-	for(x = 0; x < POPULATION_SIZE - 1; x++){
-		for(i = 0; i < POPULATION_SIZE - x - 1; i++){
+	for(x = 0; x < TAM_POPULACAO - 1; x++){
+		for(i = 0; i < TAM_POPULACAO - x - 1; i++){
 			if(scores[i].score > scores[i+1].score){
-			    scores_t temp = scores[i];
+			    pontuacao_t temp = scores[i];
 			    scores[i] = scores[i+1];
 			    scores[i+1] = temp;
 			}
@@ -170,23 +212,23 @@ void cross() {
 	//copy to best_spec array
 	int i;
 	for (i = 0; i < BEST_CNT; i++) {
-		memcpy(best_spec[i], array[scores[i].index], MOVE_LIMIT);
+		memcpy(best_spec[i], array[scores[i].index], LIMITE_MOVIMENTACAO);
 	}
 
 	int firstParent = rand() % BEST_CNT;
 	int secondParent = rand() % BEST_CNT;
-	int crossOver = rand() % MOVE_LIMIT;
+	int crossOver = rand() % LIMITE_MOVIMENTACAO;
 
-	for (i = BEST_CNT + REPRODUCTIVE; i < POPULATION_SIZE; i++) {
+	for (i = BEST_CNT + REPRODUCTIVE; i < TAM_POPULACAO; i++) {
 		memcpy(array[i], best_spec[firstParent], crossOver);
-		memcpy(array[i], best_spec[secondParent], (MOVE_LIMIT - crossOver));
+		memcpy(array[i], best_spec[secondParent], (LIMITE_MOVIMENTACAO - crossOver));
 	}
 
 }
 
 int fitness(int chromo){
     int i;
-	for(i = 0; i < MOVE_LIMIT; i++){
+	for(i = 0; i < LIMITE_MOVIMENTACAO; i++){
 		func_ptr[array[chromo][i]]();
 	}
 
@@ -198,26 +240,26 @@ int fitness(int chromo){
 
 int score(){
     int i;
-	for(i = 0; i < POPULATION_SIZE; i++){
+	for(i = 0; i < TAM_POPULACAO; i++){
 		scores[i].score = fitness(i);
 		scores[i].index = i;
 		//printf("%d %d\n", scores[i].score, scores[i].index);
 	}
-	sort_scores_t();
+	sort_pontuacao_t();
 	return scores[0].index;
 }
 //
 
 void move_player(){
     int i;
-	for(i = 0; i < MOVE_LIMIT; i++){
+	for(i = 0; i < LIMITE_MOVIMENTACAO; i++){
 		func_ptr[array[scores[0].index][i]]();
 	}
 }
 
 void mutate(){
     int i;
-	for(i = 0; i < POPULATION_SIZE-1; i++){
+	for(i = 0; i < TAM_POPULACAO-1; i++){
 		if(MUTATION_CHANCE > (double)rand() / (double)RAND_MAX){ // random number (0,1)
 			array[i][rand() % 30] = rand() % 5; // chhose random element and mutate
 		}
@@ -249,7 +291,7 @@ int main(){
 			break;
 		}
 		if(i % 100 == 0){
-			printf("generacao: %d\n", i);
+			printf("geracao: %d\n", i);
 			//move_player(); // best
 			//print_maze();
 			//player_reset();
